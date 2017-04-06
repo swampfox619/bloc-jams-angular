0dc8271b6c1687fcfb9f10f140e9752c6f6f0322
Lewis Vine <Lewis Vine>
2017-04-06 00:08:27 -0400

    assignmnet-6

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -9,11 +9,11 @@
            year: '1881',
            albumArtUrl: '/assets/images/album_covers/01.png',
            songs: [
-               { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-               { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-               { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-               { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },   
-               { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+               { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+               { title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
+               { title: 'Red', duration: 268.45, audioUrl: '/assets/music/red' },
+               { title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink' },   
+               { title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta' }
            ]   
          };
 
@@ -36,10 +36,19 @@
           return albumPicasso;  
         };
         
+        Fixtures.getCollection = function (numberOfAlbums) {
+            var albums = [];
+            counter = 0;
+            for(var i = 0; counter < numberOfAlbums; i++){
+                albums.push(albumPicasso);
+                counter++;
+            }
+            return albums;
+        };
         return Fixtures;
     }
     
     angular
-    .module('blocJams')
-    .factory('Fixtures, Fixtures');
+        .module('blocJams')
+        .factory('Fixtures', Fixtures);
 })();
